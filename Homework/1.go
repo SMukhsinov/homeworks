9a4929768bc3d2799bc624facd8d2ccfc5d454d0
Samat <muhsinov.s@gmail.com>
2021-02-17 20:41:26 +0600

    asd

--- Homework/1.go
+++ Homework/1.go
@@ -1,5 +1,16 @@
-package Homework
+package main
+
+import "fmt"
 
 func main () {
+	var a, b, c int
+	//%d chislo
+	//%s stroki
+	fmt.Scanf("%d", &a)
+	fmt.Scanf("%d", &b)
+	fmt.Scanf("%d", &c)
+	p := a + b + c
+	fmt.Println(p)
+
 
 }
--- Homework/2.go
+++ Homework/2.go
@@ -1,5 +1,13 @@
-package Homework
+package main
+
+import "fmt"
 
 func main () {
-
+	var a, b, c int
+	fmt.Scanf("%d", &a)
+	b = a + 3
+	c = b * 3
+	fmt.Printf("Первое число = %d \n", a)
+	fmt.Printf("Второе число = %d \n", b)
+	fmt.Printf("Третье число = %d \n", c)
 }
