11c5d29ac5ededeec41d0d8aeaed938f4dd5eba4
Samat <muhsinov.s@gmail.com>
2021-02-23 23:04:35 +0600

    a

--- Classwork/My homeworks/domashka 22.02/2.go
+++ Classwork/My homeworks/domashka 22.02/2.go
@@ -9,8 +9,15 @@ func main() {
 	fmt.Scanf("%d \n", &b)
 	fmt.Scanf("%d \n", &c)
 
-	if (a * a) + (b * b) = c * c && (a * a) + (c * c) = b * b && (c * c) + (b * b) = a * a{
-		fmt.Println("равностороннии треугольник")
+	//if a + b <= c || a + c <= b || b + c <= a {
+	//	fmt.Println("Треугольника не существует")
+	//}
+	if a == b && b == c && c == a {
+		fmt.Println("Равностороннии треугольник")
+	} else if a != b && a != c && b != c {
+		fmt.Println("Разностороннии треугольник")
+	} else {
+		fmt.Println("Равнобедренный треугольник")
 	}
 
 }
